ece4dbcee9217f62b994774ad247a8e8033eab91
kartikeygit11 <kartiksrivastava2307@gmail.com>
2025-06-25 22:23:54 +0530

    security configration changes

--- src/main/java/com/chat/app/config/WebConfig.java
+++ src/main/java/com/chat/app/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(frontEndUrl)
-                .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+                .allowedMethods("GET","POST","PUT","DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
     }
